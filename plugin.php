480203d7c379e252d615b8c4009c1fb34f0cbb10
Mark Brugnoli-Vinten <netniv@hotmail.com>
2025-01-02 20:42:34 +0000

    Round 2 of changes

--- plugin.php
+++ plugin.php
@@ -17,6 +17,7 @@ function plugin_passwords_install() {
         `id` int(11) NOT NULL AUTO_INCREMENT,
           `protocol` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
           `entry_id` int(11) NOT NULL,
+          `label` varchar(255) COLLATE utf8_unicode_ci NULL,
           `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
           `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
           `secret` varchar(255) COLLATE utf8_unicode_ci NULL,
@@ -38,70 +39,62 @@ function plugin_passwords_uninstall() {
 
 /* Upgrade plugin */
 function plugin_passwords_upgrade() {
-	$db_info = getPlugin ('passwords');
-	$v1 = $db_info['db_version'];
-	$code_info = plugin_plugin_info ();
-	$v2 = $code_info['version'];
+  $db_info = getPlugin('passwords');
+  $v1 = $db_info['db_version'];
+  $code_info = plugin_plugin_info();
+  $v2 = $code_info['version'];
 
-	if ($v1 == $v2)
-		throw new RackTablesError ('Versions are identical', RackTablesError::INTERNAL);
+  if ($v1 == $v2)
+    throw new RackTablesError('Versions are identical', RackTablesError::INTERNAL);
 
-	// find the upgrade path to be taken
-	$versionhistory = array
-	(
-		'0.1',
-		'0.2',
-	);
-	$skip = TRUE;
-	$path = NULL;
-	foreach ($versionhistory as $v)
-	{
-		if ($skip and $v == $v1)
-		{
-			$skip = FALSE;
-			$path = array();
-			continue;
-		}
-		if ($skip)
-			continue;
-		$path[] = $v;
-		if ($v == $v2)
-			break;
-	}
-	if ($path === NULL or ! count ($path))
-		throw new RackTablesError ('Unable to determine upgrade path', RackTablesError::INTERNAL);
+  // find the upgrade path to be taken
+  $versionhistory = array(
+    '0.1',
+    '0.2',
+  );
+  $skip = TRUE;
+  $path = NULL;
+  foreach ($versionhistory as $v) {
+    if ($skip and $v == $v1) {
+      $skip = FALSE;
+      $path = array();
+      continue;
+    }
+    if ($skip)
+      continue;
+    $path[] = $v;
+    if ($v == $v2)
+      break;
+  }
+  if ($path === NULL or ! count($path))
+    throw new RackTablesError('Unable to determine upgrade path', RackTablesError::INTERNAL);
 
-	// build the list of queries to execute
-	$queries = array ();
-	foreach ($path as $batchid)
-	{
-		switch ($batchid)
-		{
-			case '0.2':
-				// perform some upgrade step here
-        $queries[] = "ALTER TABLE `tpa_passwords` ADD `secret` varchar(255) COLLATE utf8_unicode_ci NULL";
-				$queries[] = "UPDATE Plugin SET version = '0.2' WHERE name = 'passwords'";
+  // build the list of queries to execute
+  $queries = array();
+  foreach ($path as $batchid) {
+    switch ($batchid) {
+      case '0.2':
+        // perform some upgrade step here
+        $queries[] = "ALTER TABLE `tpa_passwords` ADD `label` varchar(255) COLLATE utf8_unicode_ci NULL AFTER `entry_id`";
+        $queries[] = "ALTER TABLE `tpa_passwords` ADD `secret` varchar(255) COLLATE utf8_unicode_ci NULL AFTER `password`";
+        $queries[] = "UPDATE Plugin SET version = '0.2' WHERE name = 'passwords'";
         $refreshSecreets = true;
-				break;
-			default:
-				throw new RackTablesError ("Preparing to upgrade to {$batchid} failed", RackTablesError::INTERNAL);
-		}
-	}
+        break;
+      default:
+        throw new RackTablesError("Preparing to upgrade to {$batchid} failed", RackTablesError::INTERNAL);
+    }
+  }
 
-	// execute the queries
-	global $dbxlink;
-	foreach ($queries as $q)
-	{
-		try
-		{
-			$result = $dbxlink->query ($q);
-		}
-		catch (PDOException $e)
-		{
-			$errorInfo = $dbxlink->errorInfo();
-			throw new RackTablesError ("Query: {$errorInfo[2]}", RackTablesError::INTERNAL);
-		}
-	}
+  // execute the queries
+  global $dbxlink;
+  foreach ($queries as $q) {
+    try {
+      $result = $dbxlink->query($q);
+    } catch (PDOException $e) {
+      $errorInfo = $dbxlink->errorInfo();
+      throw new RackTablesError("Query: {$errorInfo[2]}", RackTablesError::INTERNAL);
+    }
+  }
 
   if ($refreshSecreets) {
     $qparams = array($_REQUEST['object_id']);
@@ -132,13 +125,15 @@ function plugin_passwords_upgrade() {
         $item['id']
       ];
 
-      usePreparedExecuteBlade("
+      usePreparedExecuteBlade(
+        "
         UPDATE tpa_passwords
             SET
               username = ?,
               secret = ?,
             WHERE id = ? AND secret is NULL",
-      $qparams);
+        $qparams
+      );
     }
   }
 }
@@ -166,26 +161,57 @@ function showpassword() {
     return;
   }
 
+  if (isset($_POST['showpass_x'])) {
+    $qparams = array($_REQUEST['object_id'], $_REQUEST['labelid']);
+    $query = "
+      SELECT
+        k.id as Pid,
+        k.entry_id,
+        k.username,
+        k.password,
+        k.comment,
+        k.protocol,
+        o.id
+      FROM
+        tpa_passwords k
+      LEFT JOIN
+        Object o
+      ON
+        k.entry_id=o.id
+      WHERE
+        o.id = ?
+      AND k.id = ?
+      AND k.hidden = 0;";
+
+    //$ret = array();
+    $result = usePreparedSelectBlade($query, $qparams);
+    $array = $result->fetchAll(PDO::FETCH_ASSOC);
+    $ret = [];
+    foreach ($array as $item) {
+      $ret = pdecrypt($item['secret'], 'password' . $item['object_id']);
+    }
+
+    echo json_encode($ret, JSON_PRETTY_PRINT);
+    return;
+  }
+
   if (isset($_POST['updpass_x'])) {
     // redirect to original page once query is executed
     header('Location: ' . $_SERVER['HTTP_REFERER'] . '');
-    $qparms = [
-      $_REQUEST['protocol'],
-      $_REQUEST['object_id'],
-      pencrypt($_REQUEST['username'], 'username' . $object_id),
-      pencrypt($_REQUEST['password'], 'password' . $object_id),
-      $_REQUEST['comment'],
-      $_REQUEST['labelid'],
-    ];
-    return usePreparedExecuteBlade("
-      UPDATE tpa_passwords
-          SET
-            protocol = ?,
-            entry_id = ?,
-            username = ?,
-            password = ?,
-            comment  = ?
-          WHERE id = ?", $qparms);
+    $qparms = [];
+    $qparms['label'] = $_REQUEST['label'];
+    $qparms['protocol'] = $_REQUEST['protocol'];
+    $qparms['entry_id'] = $_REQUEST['object_id'];
+    $qparms['username'] = pencrypt($_REQUEST['username'], 'username' . $object_id);
+    if (!empty($_REQUEST['password'])) {
+      $qparms['secret'] = pencrypt($_REQUEST['password'], 'password' . $object_id);
+    }
+    $qparms['comment'] = $_REQUEST['comment'];
+
+    $sql = "UPDATE tpa_passwords SET " . implode(" = ?,", array_keys($qparms)) . " = ? WHERE id = ?";
+    $qparms[] = $_REQUEST['labelid'];
+
+    return usePreparedExecuteBlade($sql, array_values($qparms));
 
     //echo print_r($_POST);
   }
@@ -193,10 +219,11 @@ function showpassword() {
 
   if (isset($_POST['addpass_x'])) {
     header('Location: ' . $_SERVER['HTTP_REFERER'] . '');
-    $qparams = [
+    $qparms = [
+      $_REQUEST['label'],
       $_REQUEST['protocol'],
-      pencrypt($_REQUEST['username'],'username' . $object_id),
-      pencrypt($_REQUEST['password'],'password' . $object_id),
+      pencrypt($_REQUEST['username'], 'username' . $object_id),
+      pencrypt($_REQUEST['password'], 'password' . $object_id),
       $_REQUEST['object_id'],
       date("Y-m-d H:i:s"),
       $remote_username,
@@ -206,6 +233,7 @@ function showpassword() {
     return usePreparedExecuteBlade("
        INSERT INTO
          tpa_passwords(
+           label,
            protocol,
            username,
            password,
@@ -213,7 +241,7 @@ function showpassword() {
            added,
            user_name,
            comment)
-       VALUES(?,?,?,?,?,?,?)", $qparms);
+       VALUES(?,?,?,?,?,?,?,?)", $qparms);
   }
 
   if (isset($_POST['delpass_x'])) {
@@ -252,54 +280,126 @@ function showpassword() {
   $result = usePreparedSelectBlade($query, $qparams);
   $array = $result->fetchAll(PDO::FETCH_ASSOC);
 
-  echo "
-   <br>
-   <br>
-   <table border=\"0\" cellpadding\"0\" cellspacing\"10\" align=\"center\">
-     <tr>
-       <td>&nbsp;</td>
-       <td>Username</td>
-       <td>Password</td>
-       <td>Protocol</td>
-       <td>Comment</td>
-       <td>&nbsp;</td>
-       ";
-  echo "<form method=\"post\" name=\"pass-add\" autocomplete=off action=\"\" style=\"display: inline;\">";
-  echo "  <tr>";
-  echo "    <td><INPUT type=\"image\"      name=\"addpass\" value=\"\" src=\"pix/tango-list-add.png\"></td>";
-  echo "    <td><input type=\"text\"       name=\"username\" value=\"\"></td>";
-  echo "    <td><input type=\"text\"       name=\"password\" value=\"\"></td>";
-  echo "    <td><input type=\"text\"       name=\"protocol\" value=\"\"></td>";
-  echo "    <td><input type=\"text\"       name=\"comment\"  value=\"\"></td>";
-  echo "    <td><INPUT type=\"image\"      name=\"addpass\"  value=\"\" src=\"pix/tango-list-add.png\"></td>";
-  echo "  </tr>";
-  echo "<input type=\"hidden\" name=\"labelid\"    value=''>";
-  echo "</form>";
-  echo "<tr><td height=\"20\"></td></tr>";
+?>
+  <br>
+  <br>
+  <table border="0" cellpadding="0" cellspacing="10" align="center">
+    <tr>
+      <td>&nbsp;</td>
+      <td>Label</td>
+      <td>Username</td>
+      <td>Password</td>
+      <td>&nbsp;</td>
+      <td>Protocol</td>
+      <td>Comment</td>
+      <td>&nbsp;</td>
+      <form method="post" name="pass-add" autocomplete=off action="" style="display: inline;">
+    <tr>
+      <td><INPUT type="image" name="addpass" value="" src="pix/tango-list-add.png"></td>
+      <td><input type="text" name="label" value=""></td>
+      <td><input type="text" name="username" value=""></td>
+      <td><input type="text" name="password" value=""></td>
+      <td>&nbsp;</td>
+      <td><input type="text" name="protocol" value=""></td>
+      <td><input type="text" name="comment" value=""></td>
+      <td><INPUT type="image" name="addpass" value="" src="pix/tango-list-add.png"></td>
+    </tr>
+    <input type="hidden" name="labelid" value=''>
+    </form>
+    <tr>
+      <td height="20"></td>
+    </tr>
+    <?php
+    foreach ($array as $key => $item) {
+      if (empty($item['secret'])) {
+        $username = $item['username'];
+        $password = $item['password'];
+        $class = "password_show";
+      } else {
+        $username = pdecrypt($item['username'], 'userame' . $item['object_id']);
 
-  foreach ($array as $key => $item) {
-    // echo $array[$key]['id'];
-    echo "<form method=\"post\" name=\"pass-" . $array[$key]['Pid'] . " \"autocomplete=off action=\"\">";
-    // ok its a dirty work around, but at least it will prevent the passwords from deleting when hitting enter
-    echo "  <INPUT type=\"image\" name=\"updpass\"  value=\"updpass\" style=\"position: absolute; left: -9999px; width: 1px; height: 1px;\"/>";
-    // and here another dirty work around, but this time from Chrome.
-    // it seems that chrome ignores autocomplete, yes...it ignores it, as workaround i have created an fake text & password field
-    // don't we just love standards....oh wait...
-    echo "   <input type=\"text\" name=\"prevent_autofill\" id=\"prevent_autofill\" value=\"\" style=\"display:none;\" />";
-    echo "   <input type=\"password\" name=\"password_fake\" id=\"password_fake\" value=\"\" style=\"display:none;\" />";
-    // end of dirty work around.
-    echo "  <tr>";
-    echo "    <td><INPUT type=\"image\"      name=\"delpass\"  value=\"\" src=\"pix/tango-list-remove.png\"></td>";
-    echo "    <td><input type=\"text\"       name=\"username\" value=\"" . pdecrypt($item['username'], 'username' . $item['object_id']) . "\"></td>";
-    echo "    <td><input type=\"password\"   name=\"password\" value=\"\"><INPUT type=\"image\" name=\"copypass\"  value=\"\" src=\"pix/tango-edit-copy-16x16.png\"></td>";
-    echo "    <td><input type=\"text\"       name=\"protocol\" value=\"" . $item['protocol'] . "\"></td>";
-    echo "    <td><input type=\"text\"       name=\"comment\"  value=\"" . $item['comment'] . "\"></td>";
-    echo "    <td><INPUT type=\"image\"      name=\"updpass\"  value=\"\" src=\"pix/tango-document-save-16x16.png\"></td>";
-    echo "  </tr>";
-    echo "<input type=\"hidden\" name=\"labelid\"    value='" . $item['Pid'] . "'>";
-    echo "</form>";
-  }
-  echo "</table>";
+        //  For now, lets show the encrypted version to how we have failed to decrypt it
+        if (empty($username)) {
+          $username = $item['username'];
+        }
+
+        $password = '';
+        $class = "password_secret";
+      }
+    ?>
+      <form method="post" name="pass-<?= $array[$key]['Pid'] ?>" autocomplete=off action="">
+        <!-- ok its a dirty work around, but at least it will prevent the passwords from deleting when hitting enter -->
+        <INPUT type="image" name="updpass" value="updpass" style="position: absolute; left: -9999px; width: 1px; height: 1px;" />
+        <!-- and here another dirty work around, but this time from Chrome. -->
+        <!-- it seems that chrome ignores autocomplete, yes...it ignores it, as workaround i have created an fake text & password field -->
+        <!-- don't we just love standards....oh wait... -->
+        <input type="text" name="prevent_autofill" id="prevent_autofill" value="" style="display:none;" />
+        <input type="password" name="password_fake" id="password_fake" value="" style="display:none;" />
+        <!-- end of dirty work around. -->
+        <tr>
+          <td><INPUT type="image" name="delpass" value="" src="pix/tango-list-remove.png"></td>
+          <td><input type="text" name="label" value="<?= htmlspecialchars($item['label'], ENT_QUOTES, 'UTF-8'); ?>"></td>
+          <td><input type="text" name="username" value="<?= htmlspecialchars($username, ENT_QUOTES, 'UTF-8'); ?>"></td>
+          <td><input type="password" name="password" value="<?= htmlspecialchars($password, ENT_QUOTES, 'UTF-8'); ?>" <?= $class ?>></td>
+          <td><INPUT type="image" name="copypass" value="" src="pix/tango-edit-copy-16x16.png"></td>
+          <td><input type="text" name="protocol" value="<?= htmlspecialchars($item['protocol'], ENT_QUOTES, 'UTF-8'); ?>"></td>
+          <td><input type="text" name="comment" value="<?= htmlspecialchars($item['comment'], ENT_QUOTES, 'UTF-8'); ?>"></td>
+          <td><INPUT type="image" name="updpass" value="" src="pix/tango-document-save-16x16.png"></td>
+        </tr>
+        <input type="hidden" name="labelid" value='<?= $item['Pid'] ?>'>
+      </form>
+    <?php
+    }
+    ?>
+  </table>
+  <script>
+    $('.password_show').on('click', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      $(this).attr('type', 'text');
+    });
+
+    $(".password_show").on('click', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      $(this).attr('type', 'password');
+    });
+
+    $('#copypass').on('click', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      var passwordValue = "";
+      var passwordField = $(this).closest("#password");
+
+      if (passwordField.length) {
+        copyToClipboard(passwordField.value());
+      } else {
+        $.ajax({
+          url: window.location.pathname + '?page=object&tab=passwords&object_id=' + object_id + "&labelid=" + label_id + "&showpass_x=1",
+          type: "get",
+        }).done(function(result) {
+          if (result.success) {
+            copyToClipboard(result.password);
+          }
+        }).fail(function() {
+          alert("Failed to retrieve password");
+        }).always(function() {
+          ajaxUIUnlock();
+        });
+      }
+    });
+
+    function copyPasswordToClipboard(passwordValue) {
+      if (typeof passwordValue !== 'undefined' && passwordValue.length) {
+        navigator.clipboard.writeText(passwordValue);
+        alert("Password copied");
+      }
+    }
+  </script>
+<?php
 }
 
 function pencrypt($message, $key, $cipher = "AES-128-CBC", $as_binary = true, $options = OPENSSL_RAW_DATA) {
@@ -376,5 +476,17 @@ function pdecrypt($ciphertext, $key, $cipher = "AES-128-CBC", $as_binary = true,
     return $original_plaintext;
   } else {
     return null;
+  }
+}
+
+function isPasswordsDebugUser() {
+  global $remote_username;
+
+  return $remote_username == 'admin';
+}
+
+function recordPasswordsDebug($message) {
+  if (isPasswordsDebugUser()) {
+    error_log("PASSWRODS: " . $message);
   }
 }
